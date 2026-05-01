465d607aae29e4b5aef3a130b58278e7614924cb
Sander69 <>
2026-05-01 15:25:31 +0000

    Lambda Functions

---
+++ section/lambda_functions/main.py
@@ -0,0 +1,8 @@
+prices = [210, 0, -891, 432, 256]
+
+apply_tax = ___ ___: max(___) * ___   # Deducting 13% tax
+
+final_prices = [apply_tax(___) for ___ in ___]
+
+# Testing the result
+print(final_prices)
